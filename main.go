94332cc44b617eb8fde41d65c2e00a2f2c97f990
Bicky Tamang <33925230+bicky-tmg@users.noreply.github.com>
2023-08-01 09:22:53 +0545

    register a handler

--- main.go
+++ main.go
@@ -4,16 +4,18 @@ import (
 	"context"
 	"fmt"
 	"go.uber.org/fx"
+	"io"
 	"net"
 	"net/http"
+	"os"
 )
 
 func main() {
-	fx.New(fx.Provide(NewHTTPServer), fx.Invoke(func(server *http.Server) {})).Run()
+	fx.New(fx.Provide(NewHTTPServer, NewServeMux, NewEchoHandler), fx.Invoke(func(server *http.Server) {})).Run()
 }
 
-func NewHTTPServer(lc fx.Lifecycle) *http.Server {
-	srv := &http.Server{Addr: ":8080"}
+func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux) *http.Server {
+	srv := &http.Server{Addr: ":8080", Handler: mux}
 	lc.Append(fx.Hook{
 		OnStart: func(ctx context.Context) error {
 			ln, err := net.Listen("tcp", srv.Addr)
@@ -29,4 +31,28 @@ func NewHTTPServer(lc fx.Lifecycle) *http.Server {
 		},
 	})
 	return srv
+}
+
+// EchoHandler is an http.Handler that copies its request body
+// back to the response.
+type EchoHandler struct{}
+
+// NewEchoHandler builds a new EchoHandler.
+func NewEchoHandler() *EchoHandler {
+	return &EchoHandler{}
+}
+
+// ServeHTTP handles an HTTP request to the /echo endpoint.
+func (*EchoHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if _, err := io.Copy(w, r.Body); err != nil {
+		fmt.Fprintln(os.Stderr, "Failed to handle request:", err)
+	}
+}
+
+// NewServeMux builds a ServeMux that will route requests
+// to the given EchoHandler.
+func NewServeMux(echo *EchoHandler) *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.Handle("/echo", echo)
+	return mux
 }
